bf7138b3e0ada79213ae93cfabf8cca5bcb0f14a
pratibha <pratibhakurumkar38@gmail.com>
2026-02-11 22:34:31 +0530

    New producr practice code added

---
+++ src/main/java/com/io/demo/ProductRest1Application.java
@@ -0,0 +1,13 @@
+package com.io.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductRest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductRest1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/io/demo/ProductRest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductRest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
